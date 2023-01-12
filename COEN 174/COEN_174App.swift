4558451f3147e1f21b521a2d2b0cbac821047d5e
gryder8 <gavin.ryder82@icloud.com>
2023-01-12 15:20:30 -0800

    Initial Commit

---
+++ COEN 174/COEN_174App.swift
@@ -0,0 +1,17 @@
+//
+//  COEN_174App.swift
+//  COEN 174
+//
+//  Created by Gavin Ryder on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct COEN_174App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
