5eaceda34ccea0772b4f14b603530f47148e5d99
gryder8 <gavin.ryder82@icloud.com>
2023-02-01 18:38:44 -0800

    First attempt at POST request for submitting review (502 error)

--- COEN 174/Models/ReviewCreator.swift
+++ COEN 174/Models/ReviewCreator.swift
@@ -18,9 +18,66 @@ class ReviewCreator {
         print("Submitting Review!")
         let apiEndpoint = baseURLString+"putReview"
         let url: URL = URL(string: apiEndpoint)!
+        print("Using url: \(url)")
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = [
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+        
+        let jsonDict = generateJSONDictForTransmission(rating: rating, text: text, title: title, reviewId: reviewId, foodId: foodId)
+        
+        do {
+            let data = try JSONSerialization.data(withJSONObject: jsonDict)
+            
+            URLSession.shared.uploadTask(with: request, from: data) { (responseData, response, error) in
+                if let error = error {
+                    print("Error with POST request: \(error)")
+                    return
+                }
+                
+                if let responseCode = (response as? HTTPURLResponse)?.statusCode, let responseData = responseData {
+                    guard responseCode == 200 else {
+                        print("Invalid response code: \(responseCode)")
+                        return
+                    }
+                    
+                    success()
+                    
+                    if let responseJSONData = try? JSONSerialization.jsonObject(with: responseData, options: .allowFragments) {
+                        print("Response JSON data = \(responseJSONData)")
+                    }
+                }
+            }.resume()
+            
+        } catch {
+            print(error)
+        }
+        
+        
         
         
         //on API call success
-        success()
+        //success()
     }
+    
+    
+    /*
+     Request: { ‘reviewId’: <optional>String, ‘userId’: String, ‘foodId’: String, ‘rating’: Num, ‘title’: <optional>String, ‘body’: <optional>String }
+     */
+    private func generateJSONDictForTransmission(rating: Int, text: String, title: String, reviewId: String, foodId: String) -> [String: Any] {
+        let userID = "Gavin"
+        let dict: [String: Any] = [
+            "reviewId":reviewId,
+            "userId":userID,
+            "foodId":foodId,
+            "rating":rating,
+            "title":title,
+            "body": text
+        ]
+        return dict
+    }
+    
+    
 }
